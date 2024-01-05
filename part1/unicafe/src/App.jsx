0fe6a4cb0e85c144fe5fa67c90ebc3f26c241454
Santeri Moilanen <santeri.moilanen@sitowise.com>
2024-01-05 16:54:24 +0200

    ex 1.6-1.7

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
+}
+
+const StatisticLine = (props) => {
+  return (
+    <div>{props.text + " " + props.value }</div>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const increaseGoodByOne = () => setGood(good + 1)
+  const increaseNeutralByOne = () => setNeutral(neutral + 1)
+  const increaseBadByOne = () => setBad(bad + 1)
+  const calculateAverage = () => {
+    return 666;
+  }
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button onClick={increaseGoodByOne} text="good" />
+      <Button onClick={increaseNeutralByOne} text="neutral" />
+      <Button onClick={increaseBadByOne} text="bad" />
+      <h1>statistics</h1>
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="neutral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
+      <StatisticLine text="all" value={good + neutral + bad} />
+      <StatisticLine text="average" value={((good - bad) / (good + neutral + bad))} />
+      <StatisticLine text="positive" value={((good / (good + neutral + bad)) * 100) + " %"} />
+    </div>
+  )
+}
+
+export default App
